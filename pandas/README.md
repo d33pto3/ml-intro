f04af80b282ebffece96e51baf0f9a74c3ffe449
d33pto3 <rakinfrhn@gmail.com>
2025-11-24 17:27:31 +0600

    initial

---
+++ pandas/README.md
@@ -0,0 +1,5 @@
+### What's covered
+
+1. CRUD for Pandas
+2. Everything we need to get up and running
+3. Crash course to get started with tabular data
---
+++ python/dictonaries.py
@@ -0,0 +1,14 @@
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates
+# Duplicate values will overwrite existing values
+
+thisDict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2024
+}
+
+print(thisDict)
+print(thisDict["brand"])
+print(len(thisDict))
