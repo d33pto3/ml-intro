0aa4a4529542c30345e51eeaee95841976e52630
d33pto3 <rakinfrhn@gmail.com>
2025-11-24 22:30:29 +0600

    fourth

--- pandas/panda.py
+++ pandas/panda.py
@@ -1,7 +1,27 @@
+# Dataframe: A 2D table of data, like excel sheet or SQL table
+# In panadas it is the main DS to store and manage tabular data
+
+
 # %%
 import pandas as pd
 
 df = pd.read_csv('./telco_churn.csv')
 
-df.head(10)
+df.tail()
+
+# %%
+tempDict = {'col1': [1, 2, 3], 'col2': [4, 5, 6], 'col3': [7, 8, 9]}
+dictdf = pd.DataFrame.from_dict(tempDict)
+
+dictdf.head()
+
+# %%
+df.columns
+# %%
+df.dtypes
+# %%
+df.describe()
+
+# %%
+df.describe(include="object")
 # %%
