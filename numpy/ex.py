8d35606500eb42e5426215f6798a839162489187
d33pto3 <rakinfrhn@gmail.com>
2025-12-03 17:28:39 +0600

    init numpy

---
+++ numpy/ex.py
@@ -0,0 +1,19 @@
+# Numpy (Numerical Python) - A Python library used for working with arrays.
+
+# 1. Create a numpy array
+
+# Numpy array-
+# a. Optimized for numerical operations (homogenous data types)
+# b. Store elements of same data type
+# c. Supports vectorized operations, which are faster
+
+# The array object in NumPy is called ndarray, it provides a lot of supporting functions that make working with ndarray very easy.
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+# ndarray
+print(type(arr))
--- pandas/panda.py
+++ pandas/panda.py
@@ -7,6 +7,13 @@ import pandas as pd
 
 df = pd.read_csv('./telco_churn.csv')
 
+## READ--------------------------------------------
+
+# Show top and bottom 5 rows
+# %%
+df.head()
+
+#%%
 df.tail()
 
 # %%
@@ -15,13 +22,98 @@ dictdf = pd.DataFrame.from_dict(tempDict)
 
 dictdf.head()
 
+# Show coulmns and data types
 # %%
 df.columns
 # %%
 df.dtypes
+
+# Create summary statistics
 # %%
 df.describe()
 
 # %%
 df.describe(include="object")
+
+# Filter on columns
 # %%
+df[["State", "International plan"]]
+
+# %%
+df.Churn.unique()
+
+# Filter on rows
+# %%
+df[df['International plan']=='Yes']
+# %%
+df[(df['International plan']=='Yes') & (df['Churn']==False)]
+
+# Indexing with iloc [row, column]
+
+#%%
+df.iloc[14]
+# %%
+df.iloc[14, 0]
+# segment of our df
+# %%
+df.iloc[22:33]
+
+# Indexing with loc
+#%%
+state = df.copy()
+state.set_index('State', inplace=True)
+# state.head()
+state.loc['OH']
+
+
+
+### UPDATE--------------------
+#%%
+df.isnull().sum()
+
+# Drop rows with missing values
+#%%
+df.dropna(inplace=True)
+
+# Drop Column
+# %%
+df.drop("Voice mail plan", axis=1)
+
+# Add new Column
+#%%
+df['New column'] = df['Total day minutes'] + df['Total intl minutes']
+
+#%%
+df.head()
+
+# Updating a single value
+#%%
+df.iloc[0, 0] = 'BS'
+
+#%%
+df['Churn Binary'] = df['Churn'].apply(lambda x: 1 if x==True else 0)
+# %%
+df[df['Churn'] == True].head()
+
+
+
+## DELETE/Output ---------------------------------
+
+#%%
+# Output to CSV
+df.to_csv('output.csv')
+
+ #%%
+json=df.to_json()
+print(json)
+
+#%%
+df.to_html()
+
+# Delet a dataframe
+#%%
+del df
+
+
+#%%
+df.head()
